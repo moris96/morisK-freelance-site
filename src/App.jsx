28c3ebaea8e6cd092778967e440480b5a15630b8
moris96 <moriskhoudari@gmail.com>
2024-02-08 11:48:13 -0500

    added pages and page transitions for cool animation when switching pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
+import React from 'react';
+import { Routes, Route, Link, useLocation } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+
+import Navbar from "./components/navbar/Navbar";
+import "./Transition.css";
 
 function App() {
+  const location = useLocation();
 
   return (
     <>
-      <h1>Hello World!</h1>
+      <Navbar />
+      <Container>
+        <TransitionGroup>
+          <CSSTransition key={location.key} classNames="page" timeout={300}>
+            <Routes location={location}>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </CSSTransition>
+        </TransitionGroup>
+      </Container>
     </>
   );
-};
+}
 
-export default App
+export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <NavbarBs sticky='top' className='navbar navbar-custom navbar-dark bg-dark'>
+      <Container>
+
+        {/* Title */}
+        <h1 className='title'>Moris K Development</h1>
+          
+        {/* Navigation */}
+        <Nav className='ms-auto'>
+          <Nav.Link to={"/"} as={NavLink}>Home</Nav.Link>
+          <Nav.Link to={"/about"} as={NavLink}>About</Nav.Link>
+        </Nav>
+      </Container>
+    </NavbarBs>
+  );
+};
+
+export default Navbar
